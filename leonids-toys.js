32c4b1fd17cae98280b7746ee67afa9fc504e1cc
warrior14 <ironaddicts14@gmail.com>
2021-11-12 17:31:15 -0600

    initial commit

---
+++ leonids-toys.js
@@ -0,0 +1,28 @@
+
+let phones = [
+    {
+        id: 1,
+        name: "iPhone",
+        maker: "Apple",
+        operatingSystem: "iOS",
+        price: 900,
+        weight: 1.2
+    },
+
+    {
+        id: 2,
+        name: "Galaxy",
+        maker: "Samsung",
+        operatingSystem: "Android",
+        price: 600,
+        weight: 1.4
+    },
+    {
+        id: 3,
+        name: "Nokia",
+        maker: "Japan",
+        operatingSystem: "Roku",
+        price: 500,
+        weight: 1.6
+    }
+]
